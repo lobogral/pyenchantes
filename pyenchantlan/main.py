3992e6c105a79f421a6007e3ad2ada0cae9574f3
Camilo Sabogal <lobogral@gmail.com>
2021-12-20 12:00:25 -0500

    Agrego la opcion de importar idiomas y paises

---
+++ pyenchantlan/main.py
@@ -0,0 +1,56 @@
+"""Agrega un país e idioma al enchant."""
+import wget
+import site
+import shutil
+import os
+import sys
+
+def descargar_diccionario(idioma: str, pais: str) -> None:
+    """
+    Hace la descarga de los correspondientes archivos .dic y .aff
+
+    Parameters
+    ----------
+    idioma
+        Idioma del diccionario
+    pais
+        País concreto del diccionario
+    """
+    
+    URL = "https://cgit.freedesktop.org/libreoffice/dictionaries/plain/"
+    wget.download(URL + '/' + idioma + '/' + pais + '.aff', pais + '.aff')
+    wget.download(URL + '/' + idioma + '/' + pais + '.dic', pais + '.dic') 
+
+def main():
+    """Método principal."""
+
+    if sys.argv[1] == "--agregar":
+        # Obtiene la carpeta donde se instalan las dependencias
+        # sea de entorno virtual o no
+        site_packages = site.getsitepackages()[1]
+        ruta_huspell = "/enchant/data/mingw64/share/enchant/hunspell/"
+        ruta_tokenize = "/enchant/tokenize/"
+        idioma = sys.argv[2]
+        pais = sys.argv[3]
+
+        if not os.path.isfile(site_packages + ruta_huspell + pais + '.dic'):
+
+            # Descarga los archivos
+            descargar_diccionario("es", "es_CO")
+
+            # Mueve los archivos
+            shutil.move(pais + '.aff',
+                        site_packages + ruta_huspell + pais + '.aff')
+            shutil.move(pais + '.dic',
+                        site_packages + ruta_huspell + pais + '.dic')
+
+            # Copia el archivo de idioma
+            if not os.path.isfile(site_packages + ruta_tokenize + idioma + ".py"):
+                shutil.copy(site_packages + ruta_tokenize + "en.py",
+                            site_packages + ruta_tokenize + idioma + ".py")
+    else:
+        os.system('pydocstyle --convention=numpy ' + sys.argv[-1])
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+def url_release(package: str) -> str:
+    """Retorna el URL faltante de un paquete.
+
+    Retorna el URL faltante de un paquete para
+    descargar el release de la última versión.
+    """
+    retorno = '/releases/latest/download/'
+    retorno += package + '.tar.gz'
+    return retorno
+
+
+setup(
+    name="pyenchantlan",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'pylint',
+        'pyenchant',
+        'wget'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyenchantlan = pyenchantlan.main:main'
+        ]
+    }
+)
